4fd30662f3efcd547a9af9bbda598ce95aed2d10
eric.benites <eric.benites@pucp.edu.pe>
2020-10-13 11:48:56 -0500

    coment

--- app/src/main/java/pe/edu/pucp/tel306/MainActivity.java
+++ app/src/main/java/pe/edu/pucp/tel306/MainActivity.java
@@ -3,6 +3,7 @@ package pe.edu.pucp.tel306;
 
 
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -45,9 +46,32 @@ public class MainActivity extends AppCompatActivity {
         //iniciarTempo.setOnClickListener(new View.OnClickListener(){
 
 
+        // if variable del reloj termina, se muestra este mensaje
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Mensaje");
+        builder.setMessage("Tiempo para un descanso");
+        builder.setPositiveButton("Ok", null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        //mensaje cuando termina el tiempo de descanso
+        AlertDialog.Builder builder2 = new AlertDialog.Builder(this);
+        builder2.setTitle("Mensaje");
+        builder2.setMessage("Here we go again");
+        builder2.setPositiveButton("Ok", null);
+        AlertDialog dialog2 = builder.create();
+        dialog2.show();
 
 
-
+        AlertDialog.Builder builder3 = new AlertDialog.Builder(this);
+        builder3.setTitle("Mensaje");
+        builder3.setMessage("Kate Winslet guarda su Oscar en el cuarto de baño " +
+                "para que sus invitados puedan sostenerlo e improvisar sus propios" +
+                " discursos de agradecimiento sin sentirse observados.");
+        builder3.setPositiveButton("Ok", null);
+        AlertDialog dialog3 = builder.create();
+        dialog3.show();
 
     }
 
